a8e44e994d16f7e8ad91dd9860bd49dead3cef79
IAN ALVES <ian.santos.alves@hotmail.com>
2023-02-14 10:43:34 -0300

    classes cloth adicionadas ao app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,16 +7,18 @@ import { UserModule } from './user/user.module';
 import { User } from './user/user.entity'
 import { LocationModule } from './location/location.module';
 import { Location } from './location/location.entity';
+import { Cloth } from './cloth/cloth.entity';
+import { ClothModule } from './cloth/cloth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'clothesDB',
-      entities: [__dirname + "/**/*.entity{.ts,.js}", User, Location],
+      entities: [__dirname + "/**/*.entity{.ts,.js}", User, Location, Cloth],
       synchronize: true,
       autoLoadEntities: true
-    }), UserModule, LocationModule
+    }), UserModule, LocationModule, ClothModule
   ],
   controllers: [AppController],
   providers: [AppService],
